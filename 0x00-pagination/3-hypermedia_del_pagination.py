81abd833d0002d4c6259a86ca8783e4c49645bfd
MarcieD1 <marciatarisai@icloud.com>
2024-06-24 22:06:02 +0200

    Deletion-resilient hypermedia pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,22 +1,58 @@
-def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-    assert 0 <= index < len(self.__indexed_dataset), "Index out of range"
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
 
-    data = []
-    current_index = index
-    next_index = index
-    items_returned = 0
+import csv
+import math
+from typing import List, Dict
 
-    while items_returned < page_size and current_index < len(self.__indexed_dataset):
-        item = self.__indexed_dataset.get(current_index)
-        if item is not None:
-            data.append(item)
-            items_returned += 1
-        next_index += 1
-        current_index += 1
 
-    return {
-        "index": index,
-        "next_index": next_index,
-        "page_size": len(data),
-        "data": data
-    }
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Returns a dictionary with the following key-value pair
+        """
+        assert index in range(len(self.indexed_dataset()))
+        next_index = index + page_size
+        data = list()
+        for i in range(index, index + page_size):
+            if not self.indexed_dataset().get(i):
+                next_index += 1
+            data.append(self.indexed_dataset().get(i))
+
+        return {
+            "index": index,
+            "next_index": next_index,
+            "page_size": page_size,
+            "data": data
+        }
